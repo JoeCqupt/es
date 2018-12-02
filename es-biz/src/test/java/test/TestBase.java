fe5a8edea95ffecc844d68592694099e844bc6fe
joecqupt <469391363@qq.com>
2018-12-03 00:57:56 +0800

    finish mybatis Generator conf

--- es-biz/src/test/java/test/TestBase.java
+++ es-biz/src/test/java/test/TestBase.java
@@ -8,6 +8,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * 测试基类
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:test-context.xml"})
+@ContextConfiguration(locations = {"classpath:applicationContext.xml"})
 public abstract class TestBase {
 }

32517ecd432ea08296f4532b3b3e65f3234f1c37
joecqupt <joe469391363@gmail.com>
2021-03-17 02:22:10 +0800

    add offer16

--- some/src/main/java/leetcode/offer/Offer16.java
+++ some/src/main/java/leetcode/offer/Offer16.java
@@ -1,26 +1,37 @@
 package leetcode.offer;
 
-/**
- *  超时： todo joe  待优化
- */
+
 public class Offer16 {
     public static void main(String[] args) {
         Offer16 o = new Offer16();
-        double v = o.myPow(2.0, -2);
+        double v = o.myPow(2.0, 10);
         System.out.println(v);
     }
+
     public double myPow(double x, int n) {
-        if(n==0){
+        if (n == 0) {
             return 1;
         }
-        if(n<0) {
-            x = 1/x;
-            n = 0-n;
-        }
 
+        // 不能使用这个思路
+//        if(n<0) {
+//            x = 1/x;
+//            n = 0-n; //有溢出风险: n取值：Integer.MIN_VALUE
+//        }
+
+        /**
+         * 思路：
+         * n为奇数： x^n = x * x^n-1
+         * n为偶数  x^n = x^2 ^ n/2
+         */
         double res = 1;
-        for(int i = 1; i<=n; i++)
-            res *= x;
-        return res;
+        while (Math.abs(n / 2) > 0) {
+            if (n % 2 != 0) {
+                res *= n < 0 ? 1 / x : x;
+            }
+            x = x * x;
+            n = n / 2;
+        }
+        return n == 1 ? x * res : (1 / x) * res;
     }
 }

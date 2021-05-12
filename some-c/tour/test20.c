2d43bd98708375449d888ad938a1e223b47c1374
蒋渊 <joe469391363@gmail.com>
2021-05-12 11:59:18 +0800

    add

---
+++ some-c/tour/test20.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+// 可以看到字符串会自动连接
+#define message "a" \
+                "b" \
+                "c" \
+                "d"
+
+#define message_for(a, b) \
+    printf(#a " and " #b ": We love you! \n")
+
+#define token_paster(n) \
+    printf("token" #n " : %d \n", token##n);
+
+// 可以判断是否有这个宏定义
+#if defined(message)
+#define message "update"
+#endif
+
+#define square(x) ((x) * (x))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
+int main()
+{
+
+    printf("message:%s \n", message);
+    message_for("joe", "yssss");
+
+    int token34 = 40;
+    token_paster(34);
+
+    printf("message:%s \n", message);
+
+    printf("square x : %d \n", square(4));
+
+    printf("MAX of %d & %d is %d \n", 5, 7, MAX(5, 7));
+    return 0;
+}
---
+++ some-c/tour/test21.c
@@ -0,0 +1,12 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE "stdio.h"
+#endif
+
+#include HEADER_FILE
+// #include <stdio.h>
+
+int main()
+{
+    printf("stdio test\n");
+    return 0;
+}
